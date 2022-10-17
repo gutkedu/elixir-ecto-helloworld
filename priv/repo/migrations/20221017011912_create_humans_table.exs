0711593e9104161cf7fe548cf6ade87caab1755f
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-10-16 22:31:47 -0300

    feat: humans table migration

---
+++ priv/repo/migrations/20221017011912_create_humans_table.exs
@@ -0,0 +1,16 @@
+defmodule Petex.Repo.Migrations.CreateHumansTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:humans) do
+      add :age, :integer
+      add :cpf, :string
+      add :email, :string
+      add :name, :string
+      timestamps()
+    end
+
+    create unique_index(:humans, [:cpf])
+    create unique_index(:humans, [:email])
+  end
+end
